97917078f9ebc580c60ff83c7acf36dec8c29226
Guduche <guduche@lburgy.ch>
2019-03-21 15:45:57 +0100

    v1.0.0
    Initial version

---
+++ options.js
@@ -0,0 +1,19 @@
+function save_options() {
+    var shaarliInstance = document.getElementById('shaarliInstance').value;
+    chrome.storage.sync.set({
+        savedShaarliInstance: shaarliInstance
+    }, function () {
+        window.close();
+    });
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        savedShaarliInstance: "https://example.com/"
+    }, function (items) {
+        document.getElementById('shaarliInstance').value = items.savedShaarliInstance;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
