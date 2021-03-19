59cd0b96165ee47089a5767b04ac8fbc09a279c5
Lucas Bürgy <contact@lburgy.ch>
2021-03-19 11:14:28 +0100

    Refactor code

--- background.js
+++ background.js
@@ -1,42 +1,42 @@
 var shaarliInstance = "https://example.com/";
 
-function addUrl(tab)
-{
-    chrome.storage.sync.get({ savedShaarliInstance: "https://example.com/"}, function (items) 
-{
-        shaarliInstance = items.savedShaarliInstance;
+function addUrl(tab) {
+  chrome.storage.sync.get({
+    savedShaarliInstance: "https://example.com/"
+  }, function (items) {
+    shaarliInstance = items.savedShaarliInstance;
     const myCode = "!function(){var o=location.href,e=document.title||o;const popup=window.open(\"" + shaarliInstance + "?post=\"+encodeURIComponent(o)+\"&title=\"+encodeURIComponent(e)+\"&description=\"+encodeURIComponent(document.getSelection())+\"&source=bookmarklet\",\"_blank\",\"menubar=no,height=800,width=600,toolbar=no,scrollbars=yes,status=no,dialog=1\")}();";
     chrome.tabs.query({
       active: true
-    }, function(tab) {
+    }, function (tab) {
       chrome.tabs.executeScript({
         code: myCode
       });
     });
   });
 }
-function addNote()
-{
-    chrome.storage.sync.get({
-        savedShaarliInstance: "https://example.com/"
-    }, function (items) {
-       shaarliInstance = items.savedShaarliInstance;
+
+function addNote() {
+  chrome.storage.sync.get({
+    savedShaarliInstance: "https://example.com/"
+  }, function (items) {
+    shaarliInstance = items.savedShaarliInstance;
     const mCode = "!function(){var o=location.href,e=document.title||o;const popup=window.open(\"" + shaarliInstance + "?post=\"+\"&description=\"+encodeURIComponent(document.getSelection()),\"_blank\",\"menubar=no,height=800,width=600,toolbar=no,scrollbars=yes,status=no,dialog=1\")}();";
     chrome.tabs.query({
       active: true
-    }, function(tab) {
+    }, function (tab) {
       chrome.tabs.executeScript({
         code: mCode
       });
     });
   });
 }
-chrome.commands.onCommand.addListener(function(command, tab) {
-	if (command == "add-url") {
-		addUrl(tab);
-		}
-		
-	if (command == "add-note") {
-		addNote();
-		}			
-      });
+chrome.commands.onCommand.addListener(function (command, tab) {
+  if (command == "add-url") {
+    addUrl(tab);
+  }
+
+  if (command == "add-note") {
+    addNote();
+  }
+});
